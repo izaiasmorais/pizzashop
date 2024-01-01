1ce145b1921fd83c81f87abe1df83724ac7f70d9
Izaias Lima <izaiaslima356@gmail.com>
2024-01-01 14:01:04 -0300

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
+export function App() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<Button>Enviar</Button>
+		</div>
+	);
+}
