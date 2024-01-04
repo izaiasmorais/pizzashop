c26af59f18d504dadb0b5e1a289ead1b22122072
Izaias Lima <izaiaslima356@gmail.com>
2024-01-04 10:32:28 -0300

    chore: set up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from "./components/ui/button";
 import "./global.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center ">
-      <Button>Enviar</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
