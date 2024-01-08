5916b4034547ae6f99d58f887d4b6842550c85b4
Izaias Lima <izaiaslima356@gmail.com>
2024-01-08 18:59:53 -0300

    chore: set up toaster

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "./global.css";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { router } from "./routes";
+import { Toaster } from "@/components/ui/sonner";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop"/>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   );
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { z } from "zod";
 
 const signInForm = z.object({
@@ -19,9 +20,18 @@ export function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+      toast.success("Enviamos um link de autenticação para seu e-mail!", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch {
+      toast.error("Credencial inválida!");
+    }
   }
 
   return (
