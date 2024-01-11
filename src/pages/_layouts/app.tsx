ad3e81931ab1589adee9d45301a95c5a6b8d916b
Izaias Lima <izaiaslima356@gmail.com>
2024-01-11 16:20:56 -0300

    chore: set up dashboard page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,12 @@
+import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
 	return (
-		<div>
-			<h1>Header</h1>
+		<div className="flex min-h-screen flex-col antialiased">
+			<Header/>
 
-			<div>
+			<div className="flex flex-1 flex-col gap-4 p-8 pt-6">
 				<Outlet/>
 			</div>
 		</div>
