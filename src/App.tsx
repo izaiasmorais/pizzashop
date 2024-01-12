2fc284afdb64a605c3963ff171bc840b918a82e1
Izaias Lima <izaiaslima356@gmail.com>
2024-01-12 10:42:58 -0300

    chore: implement dark theme system

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { router } from "./routes";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors/>
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,34 @@
 import { Home, Pizza, UtensilsCrossed } from "lucide-react";
-import { Separator} from './ui/separator';
+import { Separator } from "./ui/separator";
 import { NavLink } from "./nav-link";
+import { ThemeToggle } from "./theme/theme-toggle";
+import { AccountMenu } from "./account-menu";
 
 export function Header() {
-	return (
-		<div className="border-b">
-			<div className="flex h-16 items-center gap-6 px-6">
-				<Pizza className="h-6 w-6" />
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="h-6 w-6" />
 
-				<Separator orientation="vertical" className="h-6" />
+        <Separator orientation="vertical" className="h-6" />
 
-				<nav className="flex items-center space-x-4 lg:space-x-6">
-					<NavLink to="/">
-						<Home className="h-4 w-4" />
-						Início
-					</NavLink>
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home className="h-4 w-4" />
+            Início
+          </NavLink>
 
-					<NavLink to="/orders">
-						<UtensilsCrossed className="h-4 w-4" />
-						Pedidos
-					</NavLink>
-				</nav>
+          <NavLink to="/orders">
+            <UtensilsCrossed className="h-4 w-4" />
+            Pedidos
+          </NavLink>
+        </nav>
 
-			</div>
-
-		</div>
-	)
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
+      </div>
+    </div>
+  );
 }
