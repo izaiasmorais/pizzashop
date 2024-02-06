8acaab745767449b45e9088220d684ee27e2e5c3
Izaias Lima <izaiaslima356@gmail.com>
2024-02-05 21:52:12 -0300

    chore: create sign out request

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,15 +8,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { signOut } from "@/api/sign-out";
 
 export function AccountMenu() {
+  const navigate = useNavigate();
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
@@ -29,6 +32,13 @@ export function AccountMenu() {
       queryFn: getManagedRestaurant,
       staleTime: Infinity,
     });
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate("/sign-in", { replace: true });
+    },
+  });
 
   return (
     <Dialog>
@@ -71,11 +81,12 @@ export function AccountMenu() {
           <DropdownMenuItem
             className="text-rose-500 dark:text-rose-400"
             asChild
+            disabled={isSigningOut}
           >
-            <Link to="/sign-in">
+            <button onClick={() => signOutFn()} className="w-full">
               <LogOut className="mr-2 h-4 w-4" />
-              Sair
-            </Link>
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
