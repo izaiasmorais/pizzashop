f8b4985303133e73cdad22b75ca97c415ae4d2b8
Izaias Lima <izaiaslima356@gmail.com>
2024-02-19 19:13:30 -0300

    chore: implement automatic log out function

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,14 +1,39 @@
 import { Header } from "@/components/header";
-import { Outlet } from "react-router-dom";
+import { api } from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export function AppLayout() {
-	return (
-		<div className="flex min-h-screen flex-col antialiased">
-			<Header/>
+  const navigate = useNavigate();
 
-			<div className="flex flex-1 flex-col gap-4 p-8 pt-6">
-				<Outlet/>
-			</div>
-		</div>
-	)
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status;
+          const code = error.response?.data?.code;
+
+          if (status === 401 && code === "UNAUTHORIZED") {
+            navigate("/sign-in", { replace: true });
+          }
+        }
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
