48e791b9b21c1093b008e902cda65c6e472c8c63
Izaias Lima <izaiaslima356@gmail.com>
2024-09-08 11:11:37 -0300

    feat: set up vitest and testing libray

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { InlineConfig, type UserConfig } from "vitest/node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+	test: {
+		globals: true
 	},
-});
+}) as UserConfig & {
+	test: InlineConfig;
+};
